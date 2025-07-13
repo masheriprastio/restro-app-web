9beb33e86cb69ef4e0ddb07a4c1ed34bcfa4692e
masheriprastio <162729373+masheriprastio@users.noreply.github.com>
2025-07-13 08:28:54 +0700

    error test

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,17 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\PesananController;
+use App\Http\Controllers\HomeController;
 
+
+Route::get('/', [HomeController::class, 'index']);
 // Routing CRUD Menu
 Route::resource('menu', MenuController::class);
 
 
 
 
-Route::get('/meja', [MejaController::class, 'index'])->name('meja.index');
+Route::get('/meja', MejaController::class, 'index'])->name('meja.index');
 Route::get('/meja/create', [MejaController::class, 'create'])->name('meja.create');
 Route::post('/meja', [MejaController::class, 'store'])->name('meja.store');
 Route::get('/meja/{id}/edit', [MejaController::class, 'edit'])->name('meja.edit');
